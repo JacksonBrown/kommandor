1be41638430ec4f7c520896dc32b7ac5f0defa50
JacksonBrown <jacksonconnerbrown@gmail.com>
2016-04-26 22:59:52 -0500

    like a carousel

--- KommandorMain.cpp
+++ KommandorMain.cpp
@@ -6,7 +6,8 @@
 using namespace std;
 
 //INITAL VARIABLES
-char blue  [] = { 0x1b, '[', '1', ';', '3', '4', 'm', 0 };
+char blue  [] = { 0x1b, '[', '0', ';', '3', '6', 'm', 0 };
+char red   [] = { 0x1b, '[', '0', ';', '3', '1', 'm', 0 };
 char normal[] = { 0x1b, '[', '0', ';', '3', '9', 'm', 0 };
 char yellow[] = { 0x1b, '[', '0', ';', '3', '3', 'm', 0 };
 
@@ -15,6 +16,7 @@ void help(){
 	ifstream helpStream;
 	helpStream.open("README.md");
 	string helpVar;
+	cout << " " << endl; // gen blank line 
 
 	if ( helpStream.is_open() ){
 		while ( !helpStream.eof() ){
@@ -26,18 +28,25 @@ void help(){
 	helpStream.close();
 }
 
+// show the system users delimited by shell
 void showUsers(){
-	cout << yellow << "\nUsers with bash shell: " << normal << endl;
-	system("cat /etc/passwd | grep bash");
-	cout << "\n --- \n" << endl;
+	ifstream passwd("/etc/passwd");
 
-	cout << yellow << "\nUsers with zsh shell: " << normal << endl;
-	system("cat /etc/passwd | grep zsh");
-	cout << "\n --- \n" << endl;
+	if ( passwd ){
+		cout << yellow << "\nUsers with bash shell: " << normal << endl;
+		system("cat /etc/passwd | grep bash");
+		cout << "\n --- \n" << endl;
 
-	cout << yellow << "\nUsers with ksh shell: " << normal << endl;
-	system("cat /etc/passwd | grep ksh");
-	cout << "\n --- \n" << endl;
+		cout << yellow << "\nUsers with zsh shell: " << normal << endl;
+		system("cat /etc/passwd | grep zsh");
+		cout << "\n --- \n" << endl;
+
+		cout << yellow << "\nUsers with ksh shell: " << normal << endl;
+		system("cat /etc/passwd | grep ksh");
+		cout << "\n --- \n" << endl;
+	}else{
+		cout << red << "/etc/passwd not found." << normal << endl;
+	}
 }
 
 // PROMPT
@@ -45,15 +54,27 @@ void prompt(){
 	ifstream promptMainStream;
 	promptMainStream.open("prompt");
 	string mainPrompt;
+	cout << " " << endl;
 
 	if ( promptMainStream.is_open() ){
 		while ( !promptMainStream.eof() ){
 			getline(promptMainStream,mainPrompt);
-			cout << "\n" << blue << mainPrompt << normal << endl;
+			cout << blue << mainPrompt << normal << endl;
 		}
 	}
 
 	promptMainStream.close();
+}
+
+void editNetwork(){
+	ifstream interfaces("/etc/network/interfaces");
+
+	if( interfaces ){
+		cout << yellow << "Opening the interfaces file for editing" << normal << endl;
+		system("sudo vi /etc/network/interfaces");
+	}else{
+		cout << red << "/etc/network/interfaces file not found." << normal << endl;
+	}
 }
 
 void run_test( string x ){
@@ -63,11 +84,14 @@ void run_test( string x ){
 	else if ( x == "users" ){
 		showUsers();
 	}
+	else if ( x == "editnetwork" ){
+		editNetwork();
+	}
 } 
 
 int main(int argc, char* argv[]){
+	prompt();
 	if (argc < 2){
-		cerr << "no options are present." << endl;
 		help();
 		return 1;
 	}
--- README.md
+++ README.md
@@ -2,32 +2,4 @@ Command            | Description
 ------------       | ------------
 help    	   | Displays this
 users		   | Displays the system users
-network		   | reads the /etc/network/interfaces file
-bin		   | shows contents of bin directories
-repo		   | show the repositories in aptitude
-directory	   | display input directory
-pci		   | show all devices connected via pci
-ram		   | show all fre ram in the system
-block		   | show all block devices connected to the system
-exit		   | exit mandy console
-ports 		   | show all open ports
-setufw		   | sets up the ufw firewall to configuration
-removeuser	   | removes the specified 5 users
-removeage	   | removes and replaces the chage of 5 users
-space		   | show free disk space in the system
-groupsee 	   | view all of the suers in a specified group
-changepass	   | change pasword of specified 5 users
-editsudoremove	   | remove specified 5 users from the sudo group
-logsee		   | view the tail of input log files
-deldir		   | delete a input directory
-editservice	   | delete a input directory
-process		   | show processes on the system
-services	   | show services running on the system 
-shells		   | show shells on the system
-editssh		   | edit the sshd config file
-editlightdm	   | edit lightdm configuration files
-removereadablehome | remove world readable permissions on home directories
-dumptcp		   | view tcp packets in new window
-howdoi		   | opens google in firefox web browser
-openpam		   | views the chosen pam file
-softwarescan	   | scans for basic hacking tools
+editnetwork	   | reads the /etc/network/interfaces file
