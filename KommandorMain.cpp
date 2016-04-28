8bfba17a851908280b4a0afe752a3e53ce87e2b4
JacksonBrown <jacksonconnerbrown@gmail.com>
2016-04-28 01:21:57 -0500

    pleasehelpme

--- KommandorMain.cpp
+++ KommandorMain.cpp
@@ -12,14 +12,23 @@ char green [] = { 0x1b, '[', '0', ';', '3', '2', 'm', 0 };
 char normal[] = { 0x1b, '[', '0', ';', '3', '9', 'm', 0 };
 char yellow[] = { 0x1b, '[', '0', ';', '3', '3', 'm', 0 };
 
+class runVars{
+	public:
+		string runVar;
+};
+
+runVars rv;
+
 // PROMPT
 void help(){
 	cout << "\nUsage: " << endl;
 	cout << "help       : Displays this help prompt." << endl; 
 	cout << "users      : Displays the system users." << endl; 
 	cout << "groups     : Displays the system groups." << endl; 
-	cout << "removeuser : Completely removes a specified user.\n" << endl; 
-	cout << "memuse     : Show the system memory usage."
+	cout << "removeuser : Completely removes a specified user." << endl; 
+	cout << "memuse     : Show the system memory usage." << endl;
+	cout << "encrypt    : Encrypts a file using gpg." << endl;
+	cout << "serverstat : Display the server utilization.\n" << endl;
 }
 
 // show the system users delimited by shell
@@ -66,26 +75,79 @@ void prompt(){
 
 void removeUser( int argc, char* argv[] ){
 	cout << yellow << "removing user " << argv[2] << normal << endl;
-	string username = "sudo userdel -r " + string(argv[2]);
-	system( (username).c_str() );
+	rv.runVar = "sudo userdel -r " + string( argv[2] );
+	system( (rv.runVar).c_str() );
 	cout << " " << endl;
+}
+
+void encryptFile( int argc, char* argv[] ){
+	cout << yellow << "encrypting file " << argv[2] << normal << endl;
+	rv.runVar = "sudo gpg -c " + string( argv[2] );
+	system ( (rv.runVar).c_str() );
+	cout << " " << endl;
+}
+
+void serverUtilization( int argc, char* argv[]){
+	cout << yellow << "Displaying server utilization information: " << normal << endl;
+
+	cout << green << "Uptime: " << normal << endl;
+	system("sudo uptime");
+	cout << "\n --- \n" << endl;
+	
+	cout << green << "Users connected: " << normal << endl;
+	system("sudo who -a");
+	cout << "\n --- \n" << endl;
+
+	cout << green << "Last 3 logins: " << normal << endl;
+	system("sudo last -a | head -3");
+	cout << "\n --- \n" << endl;
+	
+	cout << green << "Most expensive processes: " << normal << endl;
+	system("sudo top -b | head -10 | tail -4");
+	cout << "\n --- \n" << endl;
+
+	cout << green << "Open ports (scanned with Nmap): " << normal << endl;
+	system("sudo nmap -sT localhost | grep -E \"(tcp|udp)\"");
+	cout << "\n --- \n" << endl;
+
+	cout << green << "Current connections: " << normal << endl;
+	system("sudo ss -s");
+	cout << "\n --- \n" << endl;
+
+
+	// IN DEVELOPMENT
+
+	/*
+	char emailopt[] = "emailadmin";
+	if ( string(argv[2]) == string(emailopt) ){
+		cout << yellow << "Emailing administrator: " << normal << endl;
+		system("kommandor serverstat | mail -s 'Kommandor serverstat output' root ");
+		cout << "OK." << endl;
+		cout << " " << endl;
+	}
+	*/
+	
 }
 
 // show system memory
 void showMemory(){
-	cout << yellow << "Displaying memory usage: " << normal << endl;
+	cout << yellow << "Displaying memory usage: \n" << normal << endl;
 
 	cout << green << "\nused memory: " << normal << endl;
 	system("vmstat -s -SM | grep used | grep memory | sed -e 's/^[ \t]*//'");
+	cout << "\n --- \n" << endl;
 
 	cout << green << "\nused swap: " << normal << endl;
 	system("vmstat -s -SM | grep used | grep swap | sed -e 's/^[ \t]*//'");
+	cout << "\n --- \n" << endl;
 
 	cout << green << "\nfree disk space: " << normal << endl;
 	system("df -h | grep -v Filesystem");
+	cout << "\n --- \n" << endl;
 
 	cout << green << "\ncpu usage: " << normal << endl;
 	system("lscpu | grep CPU");
+	cout << "\n --- \n" << endl;
 
 	cout << green << "\nload average: " << normal << endl;
 	double load[3];  
@@ -95,6 +157,7 @@ void showMemory(){
 			cout << red << "CPU load average is above 0.50!" << normal << endl;
 		}
 	}
+	cout << "\n --- \n" << endl;
 
 	cout << " " << endl;
 }
@@ -120,6 +183,12 @@ int main(int argc, char* argv[]){
 	}
 	else if ( argtest == "memuse" ){
 		showMemory();
+	}
+	else if ( argtest == "encrypt" ){
+		encryptFile( argc, argv );
+	}
+	else if ( argtest == "serverstat" ){
+		serverUtilization( argc, argv );
 	}
 	else{
 		cout << red << "\nargument not found.\n" << normal << endl;
